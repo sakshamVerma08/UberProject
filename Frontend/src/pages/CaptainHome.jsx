60c47b95d022b15854ee4ef9cb5aeb7867852ae8
Saksham Verma <sakshamverma1000@gmail.com>
2025-02-02 12:58:15 +0530

    Created a little bit of captain's home page

--- Frontend/src/pages/CaptainHome.jsx
+++ Frontend/src/pages/CaptainHome.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 const CaptainHome = () => {
   return (
-    <div>CaptainHome</div>
-  )
-}
+    <div className="h-screen ">
+      <div className="fixed top-0 p-3 flex items-center justify-between w-full">
+        <img
+          className="w-16"
+          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+          alt="Uber-Logo.png"
+        />
+        <Link
+          to="/captain-login"
+          className=" flex items-center justify-center rounded-full bg-white w-10 h-10 "
+        >
+          <i className="ri-logout-box-line"></i>
+        </Link>
+      </div>
 
-export default CaptainHome
+      <div className="h-3/5">
+        <img
+          className="h-full w-full object-cover"
+          src="https://s.wsj.net/public/resources/images/BN-XR453_201802_M_20180228165619.gif"
+          alt="temporary image"
+        />
+      </div>
+
+      <div className="h-2/5 p-6">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center justify-start gap-3">
+            <img
+              className="h-10 w-10 rounded-full object-cover"
+              src="https://plus.unsplash.com/premium_photo-1682092603230-1ce7cf8ca451?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW5kaWFuJTIwbWFufGVufDB8fDB8fHww"
+              alt="User Image"
+            />
+            <h4 className="text-lg font-medium">Harsh Patel</h4>
+          </div>
+          <div>
+            <h4 className="text-xl font-semibold ">₹295.20</h4>
+            <p className="text-sm font-medium text-gray-600">Earned</p>
+          </div>
+        </div>
+
+        <div className="flex justify-center gap-5 items-start p-5 mt-6 bg-gray-100 rounded-xl ">
+          <div className="text-center">
+            <i className="text-3xl mb-2 font-thin ri-timer-2-line"></i>
+            <h5 className="text-lg font-medium">10.2</h5>
+            <p className="text-sm text-gray-600">Hours Online</p>
+          </div>
+          <div className="text-center">
+            <i className="text-3xl mb-2 font-thin ri-speed-up-line"></i>
+            <h5 className="text-lg font-medium">10.2</h5>
+            <p className="text-sm text-gray-600">Hours Online</p>
+          </div>
+          <div className="text-center">
+            <i className="text-3xl mb-2 font-thin ri-booklet-line"></i>
+            <h5 className="text-lg font-medium">10.2</h5>
+            <p className="text-sm text-gray-600">Hours Online</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainHome;
